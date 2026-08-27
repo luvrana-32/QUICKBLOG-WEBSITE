dff22fa0792712dd6a5a94171e6c9573b6e0a6da
luvRana-32 <ranaluv569@gmail.com>
2026-08-27 12:54:20 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from "./configs/db.js";
+import dns from 'dns';
+import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
+
+
+dns.setServers(["1.1.1.1","8.8.8.8"])
+
+const app = express();
+
+await connectDB()
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res)=> res.send("API is working"))
+app.use('/api/admin', adminRouter)
+app.use('/api/blog', blogRouter)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port' + PORT)
+})
+
+export default app;
